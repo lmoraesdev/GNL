cba19308a3a322cac0e3a77a18ed14b15bb7597c
Lezzito <lbc_moraes@outlook.com>
2021-08-18 23:38:54 -0300

    start of gnl study list

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbatista <lbatista@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 11:34:26 by lbatista          #+#    #+#             */
+/*   Updated: 2021/08/18 14:16:56 by lbatista         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
